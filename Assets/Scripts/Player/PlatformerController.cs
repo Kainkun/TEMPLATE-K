cee44d52754b64ca061c27210812923b49a41cb4
Kain Shepard <kainkun100@gmail.com>
2022-01-31 01:05:31 -0800

    way better jumping logic

--- Assets/Scripts/Player/PlatformerController.cs
+++ Assets/Scripts/Player/PlatformerController.cs
@@ -18,8 +18,10 @@ public class PlatformerController : MonoBehaviour
     public bool isGrounded;
     public int maxJumps = 2;
     private int availableJumps;
-    private bool holdingJump;
+    private bool isHoldingJump;
     private bool fastFall;
+
+    private bool isFalling;
 
     public float maxSpeed = 10;
     public float timeToMaxSpeed = 0.5f;
@@ -28,14 +30,18 @@ public class PlatformerController : MonoBehaviour
     public float maxJumpHeight = 5;
     public float timeToJumpApex = 0.5f;
 
-    private Coroutine CR_coyoteTime;
+    private bool isCoyoteTime;
     public float coyoteTime = 0.2f;
-    public float coyoteTimeCounter;
-    private Coroutine CR_jumpBuffer;
-    public float jumpBuffer = 0.2f;
-    public float jumpBufferCounter;
-    public float jumpCooldown = 0.2f;
-    public float jumpCooldownCounter;
+    public float timeSinceLeftGround;
+    private bool waitingForJump;
+    public float jumpBufferTime = 0.2f;
+    public float timeSinceJumpPress;
+    private bool jumpInCooldown;
+    public float jumpCooldownTime = 0.2f;
+    public float timeSinceLastJump;
+
+    private bool inAirFromJumping;
+    private bool inAirFromFalling;
 
 
     public float fallMultiplier = 3;
@@ -58,6 +64,9 @@ public class PlatformerController : MonoBehaviour
 
         groundCheckPosition = new Vector2(0, -collider.size.y / 2);
         groundCheckSize = new Vector2(collider.size.x, groundCheckThickness);
+
+        timeSinceJumpPress = jumpCooldownTime;
+        timeSinceLastJump = jumpCooldownTime;
     }
 
     private void OnDestroy()
@@ -69,49 +78,6 @@ public class PlatformerController : MonoBehaviour
         InputManager.Get().Look -= HandleLook;
     }
 
-    IEnumerator JumpBuffer()
-    {
-        yield return new WaitForSeconds(jumpBuffer);
-        CR_jumpBuffer = null;
-    }
-
-    public void HandleJump(float value)
-    {
-        holdingJump = value > 0;
-
-        if (value > 0)
-        {
-            if (CR_jumpBuffer != null)
-                StopCoroutine(CR_jumpBuffer);
-            CR_jumpBuffer = StartCoroutine(JumpBuffer());
-        }
-    }
-
-    public void TryJump()
-    {
-        if ((availableJumps > 0 || CR_coyoteTime != null) && jumpCooldownCounter >= jumpCooldown)
-        {
-            jumpCooldownCounter = 0;
-            fastFall = false;
-            availableJumps--;
-
-            Vector2 velocity = rb.velocity;
-            velocity.y = (2 * maxJumpHeight) / timeToJumpApex;
-            rb.velocity = velocity;
-
-            if (CR_jumpBuffer != null)
-            {
-                StopCoroutine(CR_jumpBuffer);
-                CR_jumpBuffer = null;
-            }
-
-            if (CR_coyoteTime != null)
-            {
-                StopCoroutine(CR_coyoteTime);
-                CR_coyoteTime = null;
-            }
-        }
-    }
 
     public void HandleMove(Vector2 value)
     {
@@ -123,45 +89,104 @@ public class PlatformerController : MonoBehaviour
         //print(value);
     }
 
-    IEnumerator CoyoteTime()
+    public void HandleJump(float value)
     {
-        yield return new WaitForSeconds(coyoteTime);
-        CR_coyoteTime = null;
+        isHoldingJump = value > 0;
+        if (value > 0)
+            timeSinceJumpPress = 0;
+    }
+
+
+    public void Jump()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.y = (2 * maxJumpHeight) / timeToJumpApex;
+        rb.velocity = velocity;
+
+        inAirFromJumping = true;
+        inAirFromFalling = false;
+        timeSinceLastJump = 0;
+    }
+
+    public void AirJump()
+    {
+        availableJumps--;
+        Jump();
     }
 
 
     private void Update()
     {
-        jumpCooldownCounter += Time.deltaTime;
-
-        print(CR_coyoteTime != null);
-        if (CR_jumpBuffer != null)
-            TryJump();
-
+        //Jumping Logic
         bool wasGrounded = isGrounded;
         isGrounded = Physics2D.BoxCast((Vector2) transform.position + groundCheckPosition, groundCheckSize, 0, Vector2.down, 0, groundMask);
+
         if (isGrounded)
         {
             fastFall = false;
-            if(jumpCooldownCounter > jumpCooldown)
-                availableJumps = maxJumps;
+            timeSinceLeftGround = 0;
+            inAirFromJumping = false;
+            inAirFromFalling = false;
+            availableJumps = maxJumps;
+        }
+
+        //first frame landing on ground
+        if (!wasGrounded && isGrounded)
+        {
+            print("Land");
+        }
+
+        //first frame leaving ground
+        if (wasGrounded && !isGrounded)
+        {
+            print("Leave");
+
+            availableJumps--;
+
+            if (jumpInCooldown)
+                inAirFromJumping = true;
+            else
+                inAirFromFalling = true;
+        }
+
+        if (waitingForJump && !jumpInCooldown)
+        {
+            if (isGrounded || isCoyoteTime)
+                Jump();
+            else if (availableJumps > 0)
+                AirJump();
+        }
+
+        if (inAirFromFalling)
+        {
+            timeSinceLeftGround += Time.deltaTime;
+            isCoyoteTime = timeSinceLeftGround < coyoteTime;
         }
         else
         {
-            if (wasGrounded && jumpCooldownCounter > jumpCooldown)
-            {
-                wasGrounded = false;
-                print("DROP");
-                availableJumps--;
-                if (CR_coyoteTime != null)
-                    StopCoroutine(CR_coyoteTime);
-                CR_coyoteTime = StartCoroutine(CoyoteTime());
-            }
+            isCoyoteTime = false;
         }
 
+        timeSinceJumpPress += Time.deltaTime;
+        waitingForJump = timeSinceJumpPress < jumpBufferTime;
+
+        timeSinceLastJump += Time.deltaTime;
+        jumpInCooldown = timeSinceLastJump < jumpCooldownTime;
+
+
+        Vector2 velocity = rb.velocity;
+
+        //Gravity
+        //if (!isGrounded && (velocity.y < 0 || !isHoldingJump))
+        if (!isGrounded && !isHoldingJump)
+            fastFall = true;
+        float gravity = (-2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2) * Time.deltaTime;
+        if (fastFall)
+            gravity *= fallMultiplier;
+        velocity.y += gravity;
+        velocity.y = Mathf.Max(velocity.y, maxFallSpeed);
 
         //Movement
-        Vector2 velocity = rb.velocity;
         float targetSpeed = moveInputDirection.x * maxSpeed;
         float newSpeed;
         bool a = moveInputDirection.x < -0.01f && velocity.x <= 0;
@@ -171,16 +196,6 @@ public class PlatformerController : MonoBehaviour
         else
             newSpeed = Mathf.MoveTowards(velocity.x, 0, maxSpeed * (1 / timeToStop) * Time.deltaTime);
         velocity.x = newSpeed;
-
-        //Gravity
-        if (velocity.y < 0 || !holdingJump)
-            fastFall = true;
-        float gravity = (-2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2) * Time.deltaTime;
-        if (fastFall)
-            gravity *= fallMultiplier;
-        velocity.y += gravity;
-        velocity.y = Mathf.Max(velocity.y, maxFallSpeed);
-
 
         rb.velocity = velocity;
 
