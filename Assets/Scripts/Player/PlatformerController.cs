391580ea69852a1cae559082e61d6d9c540c93d8
Kain Shepard <kainkun100@gmail.com>
2022-02-03 23:25:25 -0800

    Audio and Particles

--- Assets/Scripts/Player/PlatformerController.cs
+++ Assets/Scripts/Player/PlatformerController.cs
@@ -1,10 +1,9 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Net;
+using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using Random = UnityEngine.Random;
 
+[RequireComponent( typeof(Rigidbody2D), typeof(BoxCollider2D), typeof(AudioSource) )]
 public class PlatformerController : MonoBehaviour
 {
     [Header("Movement")]
@@ -13,7 +12,7 @@ public class PlatformerController : MonoBehaviour
     public float timeToStop = 0.2f;
     public float inAirAccelerationMultiplier = 0.5f;
     public float inAirDecelerationMultiplier = 0.5f;
-    private Vector2 moveInputDirection;
+    private Vector2 _moveInputDirection;
     public AnimationCurve accelerationCurve = new AnimationCurve(new Keyframe(0, 0,0,1, 0,0.25f), new Keyframe(1, 1, 1, 0, 0.25f, 0));
     public AnimationCurve decelerationCurve = new AnimationCurve(new Keyframe(-1, 1,0,-1, 0,0.25f), new Keyframe(0, 0, -1, 0, 0.25f, 0));
     public AnimationCurve inverseAccelerationCurve;
@@ -23,161 +22,84 @@ public class PlatformerController : MonoBehaviour
     public float maxJumpHeight = 5;
     public float timeToJumpApex = 0.5f;
     public int maxJumps = 2;
-    private int availableJumps;
-    private bool fastFall;
+    private int _availableJumps;
+    private bool _fastFall;
     public float gravityMultiplier = 3;
     public float maxFallSpeed = -50;
 
     public float coyoteTime = 0.1f;
-    private bool isCoyoteTime;
-    private float timeSinceLeftGround;
+    private bool _isCoyoteTime;
+    private float _timeSinceLeftGround;
 
     public float jumpBufferTime = 0.1f;
-    private bool waitingForJump;
-    private float timeSinceJumpPress;
+    private bool _waitingForJump;
+    private float _timeSinceJumpPress;
 
     public float jumpCooldownTime = 0.2f;
-    private bool jumpInCooldown;
-    private float timeSinceLastJump;
+    private bool _jumpInCooldown;
+    private float _timeSinceLastJump;
 
-    private bool inAirFromJumping;
-    private bool inAirFromFalling;
+    private bool _inAirFromJumping;
+    private bool _inAirFromFalling;
 
     [Header("Physics")]
     public float groundCheckThickness = 0.1f;
     public LayerMask groundMask = 1;
-    private Rigidbody2D rb;
-    private BoxCollider2D boxCollider;
-    private Vector2 groundCheckPosition;
-    private Vector2 groundCheckSize;
-    private bool isGrounded;
+    private Rigidbody2D _rb;
+    private BoxCollider2D _boxCollider;
+    private Vector2 _groundCheckPosition;
+    private Vector2 _groundCheckSize;
+    private bool _isGrounded;
 
+    [Header("Effects")]
+    public GameObject airJumpParticles;
+    public GameObject groundJumpParticles;
+    public GameObject landParticles;
+
+    [Header("Audio")]
+    public AudioClip[] groundJumpSounds;
+    public AudioClip[] airJumpSounds;
+    public AudioClip[] landSounds;
+    private AudioSource _audioSource;
+    
     [Header("Other")]
     public Transform xpostrail;
     public Transform xvelrail;
 
 
-    static AnimationCurve InverseIncreasingCurve(AnimationCurve curve)
-    {
-        var reverse = new AnimationCurve();
-        for (int i = 0; i < curve.keys.Length; i++)
-        {
-            var keyframe = curve.keys[i];
-            var reverseKeyframe = new Keyframe();
-            reverseKeyframe.weightedMode = WeightedMode.Both;
-            
-            reverseKeyframe.value = keyframe.time;
-            reverseKeyframe.time = keyframe.value;
-            
-            reverseKeyframe.inTangent = 1 / keyframe.inTangent;
-            reverseKeyframe.outTangent = 1 / keyframe.outTangent;
-            
-            reverse.AddKey(reverseKeyframe);
-        }
-        for (int i = 0; i < curve.keys.Length; i++)
-        {
-            Keyframe[] reverseKeyframes = reverse.keys;
-            
-            if (i != 0)
-            {
-                float distToPrevious = curve.keys[i].time - curve.keys[i - 1].time;
-                float inHandleLengthX = curve.keys[i].inWeight * distToPrevious;
-                float inHandleLengthY = -curve.keys[i].inTangent * inHandleLengthX;
-
-                float reverseInHandleLengthX = inHandleLengthY;
-
-                float reverseDistToPrevious = reverseKeyframes[i].time - reverseKeyframes[i - 1].time;
-                reverseKeyframes[i].inWeight = -reverseInHandleLengthX / reverseDistToPrevious;
-            }
-
-            if (i != curve.keys.Length - 1)
-            {
-                float distToNext = curve.keys[i + 1].time - curve.keys[i].time;
-                float outHandleLengthX = curve.keys[i].outWeight * distToNext;
-                float outHandleLengthY = curve.keys[i].outTangent * outHandleLengthX;
-
-                float reverseOutHandleLengthX = outHandleLengthY;
-
-                float reverseDistToNext = reverseKeyframes[i + 1].time - reverseKeyframes[i].time;
-                reverseKeyframes[i].outWeight = reverseOutHandleLengthX / reverseDistToNext;
-            }
-
-            reverse.keys = reverseKeyframes;
-        }
-
-        return reverse;
-    }
-
-    static AnimationCurve InverseDecreasingCurve(AnimationCurve curve)
-    {
-        var reverse = new AnimationCurve();
-        for (int i = 0; i < curve.keys.Length; i++)
-        {
-            var keyframe = curve.keys[i];
-            var reverseKeyframe = new Keyframe();
-            reverseKeyframe.weightedMode = WeightedMode.Both;
-            
-            reverseKeyframe.value = -keyframe.time;
-            reverseKeyframe.time = -keyframe.value;
-            
-            reverseKeyframe.inTangent = 1 / keyframe.inTangent;
-            reverseKeyframe.outTangent = 1 / keyframe.outTangent;
-            
-            reverse.AddKey(reverseKeyframe);
-        }
-        for (int i = 0; i < curve.keys.Length; i++)
-        {
-            Keyframe[] reverseKeyframes = reverse.keys;
-            
-            if (i != 0)
-            {
-                float distToPrevious = curve.keys[i].time - curve.keys[i - 1].time;
-                float inHandleLengthX = curve.keys[i].inWeight * distToPrevious;
-                float inHandleLengthY = curve.keys[i].inTangent * inHandleLengthX;
-
-                float reverseInHandleLengthX = -inHandleLengthY;
-
-                float reverseDistToPrevious = reverseKeyframes[i].time - reverseKeyframes[i - 1].time;
-                reverseKeyframes[i].inWeight = reverseInHandleLengthX / reverseDistToPrevious;
-            }
-
-            if (i != curve.keys.Length - 1)
-            {
-                float distToNext = curve.keys[i + 1].time - curve.keys[i].time;
-                float outHandleLengthX = curve.keys[i].outWeight * distToNext;
-                float outHandleLengthY = curve.keys[i].outTangent * outHandleLengthX;
-
-                float reverseOutHandleLengthX = -outHandleLengthY;
-
-                float reverseDistToNext = reverseKeyframes[i + 1].time - reverseKeyframes[i].time;
-                reverseKeyframes[i].outWeight = reverseOutHandleLengthX / reverseDistToNext;
-            }
-
-            reverse.keys = reverseKeyframes;
-        }
-
-        return reverse;
-    }
-
+    public event Action OnJump;
+    public event Action OnGroundJump;
+    public event Action OnAirJump;
+    public event Action OnLand;
 
     private void Start()
     {
-        inverseAccelerationCurve = InverseIncreasingCurve(accelerationCurve);
-        inverseDecelerationCurve = InverseDecreasingCurve(decelerationCurve);
         InputManager.Get().Jump += HandleJump;
         InputManager.Get().Move += HandleMove;
         InputManager.Get().Look += HandleLook;
+        
+        _rb = GetComponent<Rigidbody2D>();
+        _boxCollider = GetComponent<BoxCollider2D>();
+        _audioSource = GetComponent<AudioSource>();
+        
+        _availableJumps = maxJumps;
+        
+        inverseAccelerationCurve = AnimCurveUtils.InverseIncreasingCurve(accelerationCurve);
+        inverseDecelerationCurve = AnimCurveUtils.InverseDecreasingCurve(decelerationCurve);
 
-        availableJumps = maxJumps;
+        Vector2 size = _boxCollider.size;
+        _groundCheckPosition = new Vector2(0, -size.y / 2);
+        _groundCheckSize = new Vector2(size.x, groundCheckThickness);
 
-        rb = GetComponent<Rigidbody2D>();
-        boxCollider = GetComponent<BoxCollider2D>();
+        _timeSinceJumpPress = jumpCooldownTime;
+        _timeSinceLastJump = jumpCooldownTime;
 
-        groundCheckPosition = new Vector2(0, -boxCollider.size.y / 2);
-        groundCheckSize = new Vector2(boxCollider.size.x, groundCheckThickness);
-
-        timeSinceJumpPress = jumpCooldownTime;
-        timeSinceLastJump = jumpCooldownTime;
+        OnGroundJump += () => _audioSource.PlayOneShot(groundJumpSounds[Random.Range(0,groundJumpSounds.Length)], 0.4f);
+        OnGroundJump += () => Instantiate(groundJumpParticles, transform);
+        OnAirJump += () => _audioSource.PlayOneShot(airJumpSounds[Random.Range(0,airJumpSounds.Length)], 0.6f);
+        OnAirJump += () => Instantiate(airJumpParticles, transform);
+        OnLand += () => _audioSource.PlayOneShot(landSounds[Random.Range(0,landSounds.Length)]);
+        OnLand += () => Instantiate(landParticles, transform.position + (Vector3)_groundCheckPosition, quaternion.identity);
     }
 
     private void OnDestroy()
@@ -190,159 +112,161 @@ public class PlatformerController : MonoBehaviour
     }
 
 
-    public void HandleMove(Vector2 value)
+    private void HandleMove(Vector2 value)
     {
-        moveInputDirection = value;
+        _moveInputDirection = value;
     }
 
-    public void HandleLook(Vector2 value)
+    private void HandleLook(Vector2 value)
     {
         //print(value);
     }
 
-    private bool jumpButtonHolding;
-    public void HandleJump(float value)
+    private bool _jumpButtonHolding;
+
+    private void HandleJump(float value)
     {
         if (value > 0)
         {
-            jumpButtonHolding = true;
-            timeSinceJumpPress = 0;
+            _jumpButtonHolding = true;
+            _timeSinceJumpPress = 0;
         }
         else
         {
-            jumpButtonHolding = false;
-            fastFall = true;
+            _jumpButtonHolding = false;
+            _fastFall = true;
         }
     }
 
 
-    public void Jump()
+    private void Jump()
     {
-        Vector2 velocity = rb.velocity;
+        Vector2 velocity = _rb.velocity;
         velocity.y = (2 * maxJumpHeight) / timeToJumpApex;
-        rb.velocity = velocity;
+        _rb.velocity = velocity;
 
-        fastFall = !jumpButtonHolding;
-        inAirFromJumping = true;
-        inAirFromFalling = false;
-        timeSinceLastJump = 0;
+        _fastFall = !_jumpButtonHolding;
+        _inAirFromJumping = true;
+        _inAirFromFalling = false;
+        _timeSinceLastJump = 0;
+        OnJump?.Invoke();
     }
 
-    public void AirJump()
+    private void GroundJump()
     {
-        availableJumps--;
         Jump();
+        OnGroundJump?.Invoke();
     }
 
-    private float t;
+    private void AirJump()
+    {
+        _availableJumps--;
+        Jump();
+        OnAirJump?.Invoke();
+    }
+
+    public PlatformerController(AudioSource audioSource)
+    {
+        this._audioSource = audioSource;
+    }
 
     private void FixedUpdate()
     {
-        // print("AAAAAAAAAAAAAAAAAAAAAAAAA");
-        //print("TIME:" + accelerationCurve.keys[1].time + "  " + "VALUE:" + accelerationCurve.keys[1].value + "  " + "IN-T:" + accelerationCurve.keys[1].inTangent + "  " + "IN-W:" + accelerationCurve.keys[1].inWeight + "  " + "OUT-T:" + accelerationCurve.keys[1].outTangent + "  " + "OUT-W:" + accelerationCurve.keys[1].outWeight);
-        inverseAccelerationCurve = InverseIncreasingCurve(accelerationCurve);
-        //print("TIME:" + inverseAccelerationCurve.keys[1].time + "  " + "VALUE:" + inverseAccelerationCurve.keys[1].value + "  " + "IN-T:" + inverseAccelerationCurve.keys[1].inTangent + "  " + "IN-W:" + inverseAccelerationCurve.keys[1].inWeight + "  " + "OUT-T:" + inverseAccelerationCurve.keys[1].outTangent + "  " + "OUT-W:" + inverseAccelerationCurve.keys[1].outWeight);
-        
-        // print("AAAAAAAAAAAAAAAAAAAAAAAAA");
-        //print("TIME:" + decelerationCurve.keys[1].time + "  " + "VALUE:" + decelerationCurve.keys[1].value + "  " + "IN-T:" + decelerationCurve.keys[1].inTangent + "  " + "IN-W:" + decelerationCurve.keys[1].inWeight + "  " + "OUT-T:" + decelerationCurve.keys[1].outTangent + "  " + "OUT-W:" + decelerationCurve.keys[1].outWeight);
-        inverseDecelerationCurve = InverseDecreasingCurve(decelerationCurve);
-        //print("TIME:" + inverseDecelerationCurve.keys[1].time + "  " + "VALUE:" + inverseDecelerationCurve.keys[1].value + "  " + "IN-T:" + inverseDecelerationCurve.keys[1].inTangent + "  " + "IN-W:" + inverseDecelerationCurve.keys[1].inWeight + "  " + "OUT-T:" + inverseDecelerationCurve.keys[1].outTangent + "  " + "OUT-W:" + inverseDecelerationCurve.keys[1].outWeight);
-
-        
         //Jumping Logic
-        bool wasGrounded = isGrounded;
-        if (rb.velocity.y > 0)
-            isGrounded = false;
+        bool wasGrounded = _isGrounded;
+        if (_rb.velocity.y > 0)
+            _isGrounded = false;
         else
-            isGrounded = Physics2D.BoxCast((Vector2) transform.position + groundCheckPosition, groundCheckSize, 0, Vector2.down, 0, groundMask);
+            _isGrounded = Physics2D.BoxCast((Vector2) transform.position + _groundCheckPosition, _groundCheckSize, 0, Vector2.down, 0, groundMask);
 
-        if (isGrounded)
+        if (_isGrounded)
         {
 
         }
 
         //first frame landing on ground
-        if (!wasGrounded && isGrounded)
+        if (!wasGrounded && _isGrounded)
         {
-            fastFall = true;
-            timeSinceLeftGround = 0;
-            inAirFromJumping = false;
-            inAirFromFalling = false;
-            availableJumps = maxJumps;
+            _fastFall = true;
+            _timeSinceLeftGround = 0;
+            _inAirFromJumping = false;
+            _inAirFromFalling = false;
+            _availableJumps = maxJumps;
+            OnLand?.Invoke();
         }
 
         //first frame leaving ground
-        if (wasGrounded && !isGrounded)
+        if (wasGrounded && !_isGrounded)
         {
-            availableJumps--;
+            _availableJumps--;
 
-            if (jumpInCooldown)
+            if (_jumpInCooldown)
             {
-                inAirFromJumping = true;
+                _inAirFromJumping = true;
             }
             else
-                inAirFromFalling = true;
+                _inAirFromFalling = true;
         }
 
-        if (waitingForJump && !jumpInCooldown)
+        if (_waitingForJump && !_jumpInCooldown)
         {
-            if (isGrounded || isCoyoteTime)
-                Jump();
-            else if (availableJumps > 0)
+            if (_isGrounded || _isCoyoteTime)
+                GroundJump();
+            else if (_availableJumps > 0)
                 AirJump();
         }
 
-        if (inAirFromFalling)
+        if (_inAirFromFalling)
         {
-            timeSinceLeftGround += Time.fixedDeltaTime;
-            isCoyoteTime = timeSinceLeftGround < coyoteTime;
+            _timeSinceLeftGround += Time.fixedDeltaTime;
+            _isCoyoteTime = _timeSinceLeftGround < coyoteTime;
         }
         else
         {
-            isCoyoteTime = false;
+            _isCoyoteTime = false;
         }
 
-        timeSinceJumpPress += Time.fixedDeltaTime;
-        waitingForJump = timeSinceJumpPress < jumpBufferTime;
+        _timeSinceJumpPress += Time.fixedDeltaTime;
+        _waitingForJump = _timeSinceJumpPress < jumpBufferTime;
 
-        timeSinceLastJump += Time.fixedDeltaTime;
-        jumpInCooldown = timeSinceLastJump < jumpCooldownTime;
+        _timeSinceLastJump += Time.fixedDeltaTime;
+        _jumpInCooldown = _timeSinceLastJump < jumpCooldownTime;
 
 
-        Vector2 velocity = rb.velocity;
+        Vector2 velocity = _rb.velocity;
 
         //Gravity
         if (velocity.y < 0)
-            fastFall = true;
+            _fastFall = true;
         float gravity = (-2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2) * Time.fixedDeltaTime;
-        if (fastFall)
+        if (_fastFall)
             gravity *= gravityMultiplier;
         velocity.y += gravity;
         velocity.y = Mathf.Max(velocity.y, maxFallSpeed);
 
         //Movement
         float percentSpeed = Mathf.Abs(velocity.x) / maxSpeed;
-        bool a = moveInputDirection.x < -0.01f && velocity.x <= 0.1f;
-        bool b = moveInputDirection.x > 0.01f && velocity.x >= -0.1f;
+        bool a = _moveInputDirection.x < -0.01f && velocity.x <= 0.1f;
+        bool b = _moveInputDirection.x > 0.01f && velocity.x >= -0.1f;
         if (a || b) //accelerate
         {
-            if (isGrounded)
+            if (_isGrounded)
                 percentSpeed = Mathf.Clamp01(inverseAccelerationCurve.Evaluate(percentSpeed) + ((1 / timeToMaxSpeed) * Time.fixedDeltaTime));
             else
                 percentSpeed = Mathf.Clamp01(inverseAccelerationCurve.Evaluate(percentSpeed) + ((1 / timeToMaxSpeed) * Time.fixedDeltaTime) * inAirAccelerationMultiplier);
             
-            velocity.x = maxSpeed * accelerationCurve.Evaluate(percentSpeed) * Mathf.Sign(moveInputDirection.x);
+            velocity.x = maxSpeed * accelerationCurve.Evaluate(percentSpeed) * Mathf.Sign(_moveInputDirection.x);
         }
         else //decelerate
         {
-            if (isGrounded)
+            if (_isGrounded)
                 percentSpeed = Mathf.Clamp01(inverseDecelerationCurve.Evaluate(-percentSpeed) - ((1 / timeToStop) * Time.fixedDeltaTime));
             else
                 percentSpeed = Mathf.Clamp01(inverseDecelerationCurve.Evaluate(-percentSpeed) - ((1 / timeToStop) * Time.fixedDeltaTime) * inAirDecelerationMultiplier);
             
             velocity.x = maxSpeed * decelerationCurve.Evaluate(-percentSpeed) * Mathf.Sign(velocity.x);
         }
-        rb.velocity = velocity;
+        _rb.velocity = velocity;
 
         //Debug trails
         Vector2 v = xpostrail.position;
@@ -351,13 +275,13 @@ public class PlatformerController : MonoBehaviour
         xpostrail.position = v;
         v = xvelrail.position;
         v.x += Time.fixedDeltaTime;
-        v.y = rb.velocity.x;
+        v.y = _rb.velocity.x;
         xvelrail.position = v;
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position + (Vector3) groundCheckPosition, groundCheckSize);
+        Gizmos.DrawWireCube(transform.position + (Vector3) _groundCheckPosition, _groundCheckSize);
     }
 }
