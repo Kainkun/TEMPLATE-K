4c0d74c4abf401c6149e4ba6a99ebe0bc10a306e
Kain Shepard <kainkun100@gmail.com>
2022-02-24 23:29:25 -0800

    collision event layers

--- Assets/Scripts/CollisionEvent.cs
+++ Assets/Scripts/CollisionEvent.cs
@@ -6,6 +6,9 @@ using UnityEngine.Events;
 
 public class CollisionEvent : MonoBehaviour
 {
+    public LayerMask collisionLayer = ~0;
+    public LayerMask triggerLayer = ~0;
+    
     public UnityEvent<Collision2D> onCollisionEnter;
     public UnityEvent<Collision2D> onCollisionExit;
     public UnityEvent<Collider2D> onTriggerEnter;
@@ -13,21 +16,25 @@ public class CollisionEvent : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        onCollisionEnter?.Invoke(other);
+        if (collisionLayer == (collisionLayer | (1 << other.gameObject.layer)))
+            onCollisionEnter?.Invoke(other);
     }
 
     private void OnCollisionExit2D(Collision2D other)
     {
-        onCollisionExit?.Invoke(other);
+        if (collisionLayer == (collisionLayer | (1 << other.gameObject.layer)))
+            onCollisionExit?.Invoke(other);
     }
     
     private void OnTriggerEnter2D(Collider2D other)
     {
-        onTriggerEnter?.Invoke(other);
+        if (triggerLayer == (triggerLayer | (1 << other.gameObject.layer)))
+            onTriggerEnter?.Invoke(other);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        onTriggerExit?.Invoke(other);
+        if (triggerLayer == (triggerLayer | (1 << other.gameObject.layer)))
+            onTriggerExit?.Invoke(other);
     }
 }
